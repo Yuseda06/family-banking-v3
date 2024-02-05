46e93aee8f755aaefb30f2a2d8cb9c8de96894cc
Yusri <muhd.yusri@rhbgroup.com>
2024-02-05 11:06:33 +0800

    adding LInear Gradient to home custom view

---
+++ components/greeting.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+import { Text, View } from "react-native";
+import { heightPercentageToDP as hp } from "react-native-responsive-screen";
+
+const GreetingText = ({ user }) => {
+  const [greeting, setGreeting] = useState("");
+
+  useEffect(() => {
+    const currentHour = new Date().getHours();
+
+    if (currentHour >= 5 && currentHour < 12) {
+      setGreeting("Good morning!");
+    } else if (currentHour >= 12 && currentHour < 18) {
+      setGreeting("Good afternoon!");
+    } else {
+      setGreeting("Good evening!");
+    }
+  }, []); // The empty dependency array ensures that this effect runs once on component mount
+
+  return (
+    <View>
+      <Text style={{ fontSize: hp(1.7), color: "white" }}>{greeting}</Text>
+
+      <Text style={{ fontSize: hp(1.7), color: "white" }} className="font-bold">
+        {user?.username}
+      </Text>
+    </View>
+  );
+};
+
+export default GreetingText;
--- components/homeHeader.js
+++ components/homeHeader.js
@@ -16,6 +16,8 @@ import {
 } from "react-native-popup-menu";
 import { AntDesign, Feather } from "@expo/vector-icons";
 import { MenuItem } from "./customMenuItems";
+import { LinearGradient } from "expo-linear-gradient";
+import GreetingText from "./greeting";
 
 const ios = Platform.OS === "ios";
 
@@ -34,71 +36,86 @@ export default function HomeHeader() {
   return (
     <View
       style={{
-        paddingTop: ios ? top : top + 10,
         shadowColor: "#000",
         shadowOffset: {
-          width: 0,
+          width: 2,
           height: 2,
         },
-        shadowOpacity: 0.35,
+        shadowOpacity: 0,
         shadowRadius: 3.84,
-        elevation: 5,
+        elevation: 20,
+        borderBottomLeftRadius: 30,
+        borderBottomRightRadius: 30,
+        overflow: "hidden", // This is important for the shadow to be visible
       }}
-      className="flex-row justify-between px-5 bg-teal-500 pb-6 rounded-b-3xl "
     >
-      <View className="pt-10 flex-row gap-4 justify-center items-center">
-        <Pressable>
-          <Feather name="menu" size={hp(3)} color="white" />
-        </Pressable>
-        <Text style={{ fontSize: hp(1.7) }} className="font-medium text-white">
-          Hello, {user?.username}
-        </Text>
-      </View>
-      <View>
-        <Menu>
-          <MenuTrigger>
-            <Image
-              style={{
-                height: hp(4.3),
-                aspectRatio: 1,
-                borderRadius: 100,
-                marginTop: 25,
+      <LinearGradient
+        colors={["#4eb1e3", "#66b2b2", "#008080"]}
+        start={{ x: 0, y: 0 }}
+        end={{ x: 1, y: 2 }}
+        style={{
+          paddingTop: ios ? top : top + 10,
+          borderBottomLeftRadius: 30,
+          borderBottomRightRadius: 30,
+          // Add any other styles you need here
+        }}
+        className="flex-row justify-between px-5  pb-6  "
+      >
+        <View className="pt-2 flex-row gap-4 justify-center items-center">
+          <Pressable>
+            <Feather name="menu" size={hp(3)} color="white" />
+          </Pressable>
+          <GreetingText user={user} />
+        </View>
+        <View>
+          <Menu>
+            <MenuTrigger>
+              <Image
+                style={{
+                  height: hp(4.3),
+                  aspectRatio: 1,
+                  borderRadius: 100,
+                  marginTop: 18,
+                  marginBottom: 10,
+                }}
+                source={user?.profileUrl}
+                placeholder={blurhash}
+                transition={1000}
+              />
+            </MenuTrigger>
+            <MenuOptions
+              customStyles={{
+                optionsContainer: {
+                  borderRadius: 10,
+                  borderCurve: "continuous",
+                  backgroundColor: "white",
+                  marginTop: 60,
+                  marginLeft: -40,
+                  width: 160,
+                  shadowOpacity: 0.35,
+                  shadowOffset: { width: 0, height: 0 },
+                },
               }}
-              source={user?.profileUrl}
-              placeholder={blurhash}
-              transition={1000}
-            />
-          </MenuTrigger>
-          <MenuOptions
-            customStyles={{
-              optionsContainer: {
-                borderRadius: 10,
-                borderCurve: "continuous",
-                backgroundColor: "white",
-                marginTop: 60,
-                marginLeft: -40,
-                width: 160,
-                shadowOpacity: 0.35,
-                shadowOffset: { width: 0, height: 0 },
-              },
-            }}
-          >
-            <MenuItem
-              text="Profile"
-              action={handleProfile}
-              value={null}
-              icon={<Feather name="user" size={hp(2.5)} color="#737373" />}
-            />
-            <Divider />
-            <MenuItem
-              text="Sign Out"
-              action={handleLogout}
-              value={null}
-              icon={<AntDesign name="logout" size={hp(2.5)} color="#737373" />}
-            />
-          </MenuOptions>
-        </Menu>
-      </View>
+            >
+              <MenuItem
+                text="Profile"
+                action={handleProfile}
+                value={null}
+                icon={<Feather name="user" size={hp(2.5)} color="#737373" />}
+              />
+              <Divider />
+              <MenuItem
+                text="Sign Out"
+                action={handleLogout}
+                value={null}
+                icon={
+                  <AntDesign name="logout" size={hp(2.5)} color="#737373" />
+                }
+              />
+            </MenuOptions>
+          </Menu>
+        </View>
+      </LinearGradient>
     </View>
   );
 }
